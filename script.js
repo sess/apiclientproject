9e993be4108fa7078d128f1d3e1e850752be0ff9
sergio <serj.sanch@gmail.com>
2019-03-23 00:48:36 +0000

    updates

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
 /* global $ */
 
+let currentTurn = "player";
+
+let currentCard = "none";
+
+let previousTurn = "computer";
+
+let compMatches = 0;
+
+let playerMatches = 0;
+
+let win = false;
+
+function Turns(){
+    if(currentTurn === "player"){
+        currentTurn = "computer";
+    }else if(currentTurn === "computer"){
+        currentTurn = "player";
+    }
+}
+
+function comLogic(){
+    
+}
+
 $.ajax({
-    url: "https://deckofcardsapi.com/api/deck/lsk54361eevo/shuffle/?deck_count=1",
+    url: "https://deckofcardsapi.com/api/deck/k6md5slc2b2s/shuffle/?deck_count=1",
     method: "GET",
     success: function(response){
         var deck_id = response.deck_id;
+        console.log(deck_id);
         $(".cardCount").text(response.remaining);
         
-    $.ajax({
-        url:"https://deckofcardsapi.com/api/deck/" + deck_id +"/draw/?count=5",
-        method: "GET",
-        success: function(response){
-            response.cards.forEach(function(cardIMG){
-                $(".playerHand").append("<img class='cards' class='playerCards' src='" + response.cards.image + "'>");
-            });        
-        }
-    });
+        $.ajax({
+            url:"https://deckofcardsapi.com/api/deck/k6md5slc2b2s/draw/?count=5",
+            method: "GET",
+            success: function(response){
+                for (let i = 0; i < 5; i++){
+                    console.log(i);
+                    $("#playerHand").append("<button class=\"cardBttns\" id=\"pCard\"><img class=\"cards\" id=\"playerCards\" src=\"" + response.cards[i].images.png + "\"></button>");
+                    $(".cardCount").text(response.remaining);
+                }        
+            }
+        });
     
-    $.ajax({
-        url:"https://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=5",
-        method: "GET",
-        success: function(response){
-            response.cards.forEach(function(){
-                $(".opponentHand").append(`<img class="cards" class="opponentCards" src='"${response.cards.images}"'>`);
-            });        
-        }
-    });
-    $(".cardCount").text(response.remaining);
+        $.ajax({
+            url:"https://deckofcardsapi.com/api/deck/k6md5slc2b2s/draw/?count=5",
+            method: "GET",
+            success: function(response){
+                for (let q = 0; q < 5; q++){
+                    $("#opponentHand").append("<button clickable=\"false\"class=\"cardBttns\"><img class=\"cards\" id=\"opponentCards\" id=\"" + q + "\" src=\"cardBackside.png\"></button");
+                    $(".cardCount").text(response.remaining);
+                }        
+            }
+        });
     }
+});
+
+$("#pCard0").click(function(){
+ 
 });
